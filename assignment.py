e4de97a65880f42843823548cfaf1033a8ad8784
Magdalena440 <tree_magdalena@student.ceu.edu>
2026-09-23 17:15:41 +0200

    commets and predictions

--- assignment.py
+++ assignment.py
@@ -1,6 +1,7 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+#My predictions and comments
+drink = "tea" #assigns string to variable
+sales = {"tea": 3, "coffee": 5} #assigns numbers to strings, sales contains two strings with assigned numbers
+sold = sales[drink] #indicates the number for tea (which is 3)
+sold = sold + 1 #sold gets overwritten, it is now 4 and not 3
+print(drink) #prints tea
+print(sold) #prints 4
